1ef37a0bc87720b064df85484c1bf2169ce877cb
Altered Art <cherokeedreemer@gmail.com>
2020-10-12 10:33:37 -0600

    Updates

---
+++ precache-manifest.5b743f34819a10c5535e6f021933fdbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e93a535ae607919611508eb3a760015",
+    "url": "/Shiny-Pokedex/index.html"
+  },
+  {
+    "revision": "a0223bda1265627d687b",
+    "url": "/Shiny-Pokedex/static/css/main.2741ac3f.chunk.css"
+  },
+  {
+    "revision": "3a7f5e40fe3bc9d90cb4",
+    "url": "/Shiny-Pokedex/static/js/2.7e17f7b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shiny-Pokedex/static/js/2.7e17f7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0223bda1265627d687b",
+    "url": "/Shiny-Pokedex/static/js/main.17fe4465.chunk.js"
+  },
+  {
+    "revision": "4ab96facf512f093c91b",
+    "url": "/Shiny-Pokedex/static/js/runtime-main.a4648c32.js"
+  }
+]);
